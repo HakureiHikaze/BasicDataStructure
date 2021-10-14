0bbfab6c5d79424099ba7b3c85ff6fa1b5c12db3
HakureiHikaze <wgyaaa123@gmail.com>
2021-10-15 01:33:32 +0800

    dev

---
+++ BitStream.c
@@ -0,0 +1,5 @@
+//
+// Created by Hikaze on 2021/10/12.
+//
+
+#include "BitStream.h"
--- Tensor.c
+++ Tensor.c
@@ -7,13 +7,14 @@
 
 Tensor* TensorInit(const size_t* pDimensionWidth, size_t dimensionCount){
     Tensor* pRtn = (Tensor*) calloc(1, sizeof (Tensor));
-    pRtn->pDimensionWidth = (size_t*) calloc(dimensionCount, sizeof(size_t));
+    pRtn->pDimensionWidth = (size_t*) calloc(dimensionCount+1, sizeof(size_t));
     ASSERT(pRtn&& pRtn->pDimensionWidth);
     size_t length = 1;
     for(size_t i = 0; i<dimensionCount;i++){
         length*=pDimensionWidth[i];
         pRtn->pDimensionWidth[i] = pDimensionWidth[i];
     }
+    pRtn->pDimensionWidth[dimensionCount] = length;
     pRtn->aData = (long double*) calloc(length, sizeof(long double));
     pRtn->dimensionCount = dimensionCount;
     return pRtn;
